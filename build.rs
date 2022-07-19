d9203bc965d6eb4001faa53e2b931c735c78d90d
wangdong.teshin <wangdong.teshin@bytedance.com>
2022-07-19 18:49:55 +0800

    初始化项目

---
+++ build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    tonic_build::configure()
+        .out_dir("./src/pb")
+        .file_descriptor_set_path("./src/pb/services_descriptor.bin")
+        // .type_attribute("hello.HelloResponse","#[derive(serde::Serialize,serde::Deserialize)]")
+        // .type_attribute("hello.HelloRequest","#[derive(serde::Serialize,serde::Deserialize)]")
+        .compile(&["./proto/message.proto","./proto/services.proto"],&["proto"])
+        .unwrap();
+}
